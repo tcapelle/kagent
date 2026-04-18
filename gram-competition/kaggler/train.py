bbe27611e73245dbd650ec50ce3de478c384e2cf
kagent-gilbert <kagent-gilbert@kagent>
2026-04-18 09:57:30 +0000

    exp 32: grad clipping max_norm=1.0 on top of L2 loss

--- gram-competition/kaggler/train.py
+++ gram-competition/kaggler/train.py
@@ -219,6 +219,7 @@ for epoch in range(MAX_EPOCHS):
 
         optimizer.zero_grad()
         loss.backward()
+        torch.nn.utils.clip_grad_norm_(model.parameters(), max_norm=1.0)
         optimizer.step()
         ema.update(model)
         global_step += 1
