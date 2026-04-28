7cb33694fcd36a13a1afc89f681e8efe4ae8bbf1
kagent-thorfinn <kagent-thorfinn@kagent>
2026-04-28 00:13:55 +0000

    router_meta: add nezuko_934

--- tandemfoil-competition/kaggler/router_meta.py
+++ tandemfoil-competition/kaggler/router_meta.py
@@ -70,6 +70,7 @@ SRC = {
     "nezuko_b0e": ("nezuko",  "b0e0889"),  # 34.61! (single 35.03, rc 48.61, cr 20.41, re 34.38)
     "nezuko_aafa": ("nezuko", "aafa24b"),  # 34.595
     "nezuko_b0b": ("nezuko",  "b0bbb68"),  # 34.582
+    "nezuko_934": ("nezuko",  "93428ec"),  # 34.578
     "tanjiro4":   ("tanjiro", "ad4711d"),  # 37.44 (improved tanjiro)
     "nezuko_85":  ("nezuko",  "85845f4"),  # 35.195693
     "nezuko_abe": ("nezuko",  "abe700a"),  # 35.195697
